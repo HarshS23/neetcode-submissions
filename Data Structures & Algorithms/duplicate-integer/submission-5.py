3aef7865f6c6ea1198c8fe73bf018a6c5b99d818
Adharsh Shokkalingam <86747648+HarshS23@users.noreply.github.com>
2026-05-29 13:56:28 -0600

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.py
@@ -0,0 +1,40 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        for i in nums:
+            j = i + 1 
+            if (i == j):
+                return True 
+            elif (j == None):
+                return False 
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # nums.sort() # sort the array that takes O(nlogn)
+        # for i in range(len(nums) - 1): # lopp takes O(n) 
+        #     if nums[i] == nums[i+1]: # O(1)
+        #         return True # O(1)
+        # return False # O(1)
