bdabba09f824f2107e6758d6bbed5ea94ff517d6
Adharsh Shokkalingam <86747648+HarshS23@users.noreply.github.com>
2026-06-03 17:02:56 -0600

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.py
@@ -0,0 +1,44 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        # Using a Hashmap 
+        hashset = set()
+
+
+
+        # nums.sort() # must be sorted first O(nlog) - worst case
+        # for i in range(len(nums) - 1): # check until the end but not i +1 O(n)
+        #     j = i + 1 
+        #     if (nums[i] == nums[j]): # O(1)
+        #         return True 
+        # return False 
+
+        # # so in total the run time is O(n log n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # nums.sort() # sort the array that takes O(nlogn)
+        # for i in range(len(nums) - 1): # lopp takes O(n) 
+        #     if nums[i] == nums[i+1]: # O(1)
+        #         return True # O(1)
+        # return False # O(1)
